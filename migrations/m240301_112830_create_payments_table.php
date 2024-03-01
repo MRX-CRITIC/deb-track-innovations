0c671c8ebed3f31086bb4c3a6790b6dfe4cc22b3
DarkToL <botinok30@gmail.com>
2024-03-01 18:59:31 +0400

    Изменения на занятие 01/03/2024 - 19:00

--- migrations/m240301_112830_create_payments_table.php
+++ migrations/m240301_112830_create_payments_table.php
@@ -11,11 +11,25 @@ class m240301_112830_create_payments_table extends Migration
         $this->createTable('{{%payments}}', [
             'id' => $this->primaryKey(),
             'operation_id' => $this->integer()->notNull(),
+            'start_date_billing_period' => $this->date(),
+            'end_date_billing_period' => $this->date(),
+            'date_payment' => $this->date(),
         ]);
+
+        $this->addForeignKey(
+            'fk-payments-operation_id',
+            'payments',
+            'operation_id',
+            'operations',
+            'id',
+            'CASCADE'
+        );
     }
 
     public function safeDown()
     {
+//        $this->dropForeignKey('fk-payments-operation_id', 'payments');
 
+        $this->dropTable('payments');
     }
 }
